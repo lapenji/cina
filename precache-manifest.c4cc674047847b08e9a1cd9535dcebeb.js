190f7b9c0553d9cfbc674a21ff48a4f4d27944a1
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-04-30 14:28:10 +0200

    Updates

---
+++ precache-manifest.c4cc674047847b08e9a1cd9535dcebeb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d704bae55fc3691bb7fdb3df121ce4be",
+    "url": "/cina/index.html"
+  },
+  {
+    "revision": "4ba0878399341b71cd63",
+    "url": "/cina/static/css/main.4d01989d.chunk.css"
+  },
+  {
+    "revision": "567a10ec5c33d89b09c7",
+    "url": "/cina/static/js/2.54f657e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cina/static/js/2.54f657e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba0878399341b71cd63",
+    "url": "/cina/static/js/main.c49298d1.chunk.js"
+  },
+  {
+    "revision": "bd4533cee5f4ee87aa3f",
+    "url": "/cina/static/js/runtime-main.f1e194e9.js"
+  },
+  {
+    "revision": "a9a07d89293efb75a57bb31873d70330",
+    "url": "/cina/static/media/cina.a9a07d89.jpeg"
+  },
+  {
+    "revision": "20197d04274470352543b56aab795372",
+    "url": "/cina/static/media/cina2.20197d04.png"
+  },
+  {
+    "revision": "2619645c8ac435b9b9ab92378a042a52",
+    "url": "/cina/static/media/icona.2619645c.png"
+  }
+]);
